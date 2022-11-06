f872d9246275b2ce0c61b34bd5426c9881ba3496
Dawid Łakomy <dawid.lakomy@oracle.com>
2022-11-06 11:10:13 +0100

    Fix label and filename

--- src/test/scala/parser/BaseParserSpec.scala
+++ src/test/scala/parser/BaseParserSpec.scala
@@ -7,7 +7,7 @@ import org.scalatest.flatspec.AnyFlatSpec
 
 abstract class BaseParserSpec(parserFn: (String, ParserConfig) => ParseResult) extends AnyFlatSpec:
 
-  "The listener based parser" should "return an expected output" in {
+  "The parser" should "return an expected output" in {
     val output = parserFn(
       exampleInput,
       ParserConfig(
