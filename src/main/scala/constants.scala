622c52b91cc7284c832a35bf4b2dd10e5b117bdb
Dawid Łakomy <dawid.lakomy@oracle.com>
2022-11-06 10:50:05 +0100

    Move exapmles to constants

--- src/main/scala/constants.scala
+++ src/main/scala/constants.scala
@@ -1,10 +1,12 @@
 package antlrPlayground
 package constants
 
+import model._
+
 // anonymised input
 // I assume, for simplicity, that the outpur looks almost exactly like that
 // ie. p_txt is after p_name, and there is no other commands
-val exampleInput =
+val exampleInput: String =
 """BEGIN
   import_utils.add_msg(
     p_id=>utils.id(1232)
@@ -27,6 +29,12 @@ val exampleInput =
   );
 END;
 """
+
+val expectedOutput: ParseResult = Map(
+  "'MSG_WITH_CONCAT'" -> """unistr('concat part1 in function')||unistr(' "part two in quotes, after linebreak, in function"')""",
+  "'TASK.DEF_NOT_FOUND'" -> "'Quite simple msg'",
+  "'MSG3'" -> "string_utils.join(to_varchar2('part1'),to_varchar2('part2'))"
+)
 
 val fnName = "import_utils.add_msg"
 val keyArgName = "p_name"
